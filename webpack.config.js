68ae10421080573552a9ca44353b17e7513b9dd5
Mohammad Hefny <mohammad.hefny@gmail.com>
2024-07-30 02:52:17 +0300

    package versions

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,27 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   // ... other Webpack configuration
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new UglifyJsPlugin({
+        parallel: true,
+        sourceMap: true,
+        mangle: true,
+      }),
+    ],
+  },
+  
   plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[cc].css",
+    }),
     new UglifyJSPlugin({
       uglifyOptions: {
         mangle: {
           keep_fnames: false,
           keep_classnames: false,
+          keep_fnames: false
         },
         compress: {
           pure_getters: true,
