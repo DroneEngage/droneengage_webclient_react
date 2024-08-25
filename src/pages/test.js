3e73ee082b06181f4bde51e898b76854274009f1
Mohammad Hefny <mohammad.hefny@gmail.com>
2024-08-25 08:40:03 +0300

    testing

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,7 +1,7 @@
 import React , { useEffect } from 'react';
 
 
-import ClassBarChart from '../components/gadgets/jsc_ctrl_bar_chart'
+import {ClassRadar, highlightSection} from '../components/test/jsc_radar';
 
 
 
@@ -12,13 +12,11 @@ const Test = () => {
 	}
 	);
   
-    const data = [50, 75, 120, 90, 80,50, 75, 120, 90, 80,50, 75, 120, 90, 80];
-    const labels = ['Jan', 'Feb', 'Mar', 'Apr', 'May'];
-
+    
 
   	return (
-    <div id='myChart'>
-        <ClassBarChart data={data} labels={labels} />
+    <div>
+      <ClassRadar N={8} M={4} />
     </div>
     )
   };
