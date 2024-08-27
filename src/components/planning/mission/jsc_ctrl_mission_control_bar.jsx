fc7a1f8426a34dc99d8f79983557ba44654c935f
Mohammad Hefny <mohammad.hefny@gmail.com>
2024-08-27 11:35:34 +0300

    fix: upload mission2

--- src/components/planning/mission/jsc_ctrl_mission_control_bar.jsx
+++ src/components/planning/mission/jsc_ctrl_mission_control_bar.jsx
@@ -5,7 +5,8 @@ import {js_globals} from '../../../js/js_globals.js';
 
 import {
     fn_requestWayPoints,
-    fn_clearWayPoints, fn_putWayPoints 
+    fn_clearWayPoints, 
+    fn_do_modal_confirmation,
     } from '../../../js/js_main.js'
 
 
@@ -39,10 +40,34 @@ export class ClssMission_Control_Bar extends React.Component {
 
     fn_putWayPoints(p_partyID, p_eraseFirst)
     {
-        // fn_putWayPoints in js_main
         const v_andruavUnit = js_globals.m_andruavUnitList.fn_getUnit(p_partyID);
+        if (v_andruavUnit===null) return ;
+
         const c_mission_text = this.props.p_mission.fn_exportToV110 ();
-        js_globals.v_andruavClient.API_uploadWayPoints(v_andruavUnit, p_eraseFirst, c_mission_text);
+        
+        fn_do_modal_confirmation("Upload Mission for " + v_andruavUnit.m_unitName,
+            "Are you sure you want to upload mission?", function (p_approved) {
+                if (p_approved === false) return;
+                // fn_putWayPoints in js_main
+                js_globals.v_andruavClient.API_uploadWayPoints(v_andruavUnit, p_eraseFirst, c_mission_text);
+
+            }, "YES", "bg-danger text-white");
+        
+    }
+
+
+    fn_requestWayPoints(p_partyID, p_fromFCB)
+    {
+        const v_andruavUnit = js_globals.m_andruavUnitList.fn_getUnit(p_partyID);
+        if (v_andruavUnit===null) return ;
+        fn_requestWayPoints(v_andruavUnit, p_fromFCB);
+    }
+
+    fn_clearWayPoints(p_partyID, p_fromFCB)
+    {
+        const v_andruavUnit = js_globals.m_andruavUnitList.fn_getUnit(p_partyID);
+        if (v_andruavUnit===null) return ;
+        fn_clearWayPoints(v_andruavUnit, p_fromFCB);
     }
 
     render ()
@@ -54,8 +79,8 @@ export class ClssMission_Control_Bar extends React.Component {
                 <button  id='pre_geo_btn_generate' key={'mp1b1' + c_key} className='btn btn-primary btn-sm ctrlbtn'   title ="Export Mission as File" type="button "  onClick={ (e) => this.fn_exportMission(e) } >Export</button>
                 <button  id='geo_btn_georeset'  key={'mp1b2' + c_key} className="btn btn-warning btn-sm ctrlbtn" title ="Reset Mission on Map" type="button" onClick={ (e) => this.fn_deleteMission(e) } >Reset</button>
                 <button  id='geo_btn_geoupload'  key={'mp1b3' + c_key} className="btn btn-danger btn-sm ctrlbtn" title ="Save Mission on Unit" type="button" onClick={ (e) => this.fn_putWayPoints(this.props.m_selected_unit,true)}  >Upload</button>
-                <button  id='geo_btn_georead'  key={'mp1b4' + c_key} className="btn btn-warning btn-sm ctrlbtn" title ="Read Mission from Unit" type="button" onClick={ (e) => fn_requestWayPoints(this.props.m_selected_unit,true)} >Read</button>
-                <button  id='geo_btn_geoclear'  key={'mp1b5' + c_key} className="btn btn-danger btn-sm ctrlbtn" title ="Delete Mission from Unit" type="button" onClick={ (e) => fn_clearWayPoints(this.props.m_selected_unit, true) } >Clear</button>
+                <button  id='geo_btn_georead'  key={'mp1b4' + c_key} className="btn btn-warning btn-sm ctrlbtn" title ="Read Mission from Unit" type="button" onClick={ (e) => this.fn_requestWayPoints(this.props.m_selected_unit,true)} >Read</button>
+                <button  id='geo_btn_geoclear'  key={'mp1b5' + c_key} className="btn btn-danger btn-sm ctrlbtn" title ="Delete Mission from Unit" type="button" onClick={ (e) => this.fn_clearWayPoints(this.props.m_selected_unit, true) } >Clear</button>
             </div>
         );
     }
