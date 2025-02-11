fd6f71735eea977834fc52ce9af67ce4cefd0728
HefnySco <mohammad.hefny@gmail.com>
2025-02-11 05:00:48 +0300

    fix: using shouldCOmponentUpdate to reduce calls for ClssCtrlDirections

--- src/components/gadgets/jsc_ctrl_directionsControl.jsx
+++ src/components/gadgets/jsc_ctrl_directionsControl.jsx
@@ -1,21 +1,74 @@
 import $ from 'jquery'; 
 import React    from 'react';
 
+import {js_globals} from '../../js/js_globals.js';
+import {js_eventEmitter} from '../../js/js_eventEmitter.js'
+
 export class ClssCtrlDirections extends React.Component {
 
-    constructor()
+    constructor(props)
 	{
-		super ();
-		    this.state = {
+		super (props);
+		
+        this.state = {
+            'm_update': 0
 		};
+
+        this.key = Math.random().toString();
+        this.m_dirRef = React.createRef();
+                
+        js_eventEmitter.fn_subscribe(js_globals.EE_unitNavUpdated, this, this.fn_update);
+    
     }
 
     
 
+   shouldComponentUpdate(nextProps, nextState) {
+        const update = (this.state.m_update != nextState.m_update) ;
+
+        return update;
+    }
+
+
+    componentWillUnmount () {
+        js_eventEmitter.fn_unsubscribe (js_globals.EE_onProxyInfoUpdated,this);
+    }
+
+    
+
+    componentDidMount() {
+        this.state.m_update = 1;
+    }
+
+
+    componentDidUpdate() {
+        const v_nav_info = this.props.p_unit.m_Nav_Info;
+        const c_target = v_nav_info._Target.target_bearing ; 
+        const c_bearing = v_nav_info.p_Desired.nav_bearing ;
+        this.draw(c_target,c_bearing);
+    }
+
+    
+    fn_update (p_me,p_andruavUnit)
+    {
+        try
+        {
+            if (p_me.props.p_unit.partyID !== p_andruavUnit.partyID) return ;
+
+            if (p_me.state.m_update === 0) return ;
+            p_me.setState({'m_update': p_me.state.m_update +1});
+        }
+        catch (ex)
+        {
+
+        }
+    }
+
+    
     draw (target_deg, bearing_deg) 
     {
         //fn_console_log ("target_deg:"+ target_deg + " bearing_deg:" + bearing_deg);
-        const c_canvas=$('#' + this.props.id +' #ctrl_target_bearing')[0];
+        const c_canvas=this.m_dirRef.current; //$('#' + this.props.id +' #ctrl_target_bearing')[0];
         const c_ctx = c_canvas.getContext('2d');
         
         c_canvas.width  = 50;
@@ -72,20 +125,12 @@ export class ClssCtrlDirections extends React.Component {
     }
 
 
-    componentDidMount() {
-        this.draw(this.props.v_target,this.props.v_bearing);
-    }
-
-    componentDidUpdate() {
-        this.draw(this.props.v_target,this.props.v_bearing);
-    }
-
     render ()
     {
         return (
-            <div  id={this.props.id}  className='css_hud_div'>
+            <div key={this.key + 'dir'}  className='css_hud_div'>
                 <div className = 'row al_l css_margin_zero'>
-                    <canvas id='ctrl_target_bearing' className='css_target_bearing'></canvas>
+                    <canvas key={this.key + 'cdir'} id='ctrl_target_bearing' ref={this.m_dirRef} className='css_target_bearing'></canvas>
                 </div>
             </div>
         )
