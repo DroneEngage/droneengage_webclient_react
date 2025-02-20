93fc00e86cbc8607ba4cc2d80c73c829fdbef6a6
HefnySco <mohammad.hefny@gmail.com>
2025-02-20 03:49:45 +0300

    fix path

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express();
 const sslOptions = {
   //cert: fs.readFileSync(process.env.WEB_SSL_CRT),
   //key: fs.readFileSync(process.env.WEB_SSL_KEY),
-  cert: fs.readFileSync("/home/mhefny/TDisk/public_versions/andruav/andruav_webclient_2/ssl/localssl.crt"),
-  key: fs.readFileSync("/home/mhefny/TDisk/public_versions/andruav/andruav_webclient_2/ssl/localssl.key"),
+  cert: fs.readFileSync("./ssl/localssl.crt"),
+  key: fs.readFileSync("./ssl/localssl.key"),
 };
 
 https.createServer(sslOptions, app).listen(3000, () => {
