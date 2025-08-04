b244f305d212dd270075b88c63ff9c5cf451e458
HefnySco <mohammad.hefny@gmail.com>
2025-08-04 06:09:37 +0300

    fix logic: check list update

--- src/components/gadgets/jsc_ctrl_tracker_ai_list.jsx
+++ src/components/gadgets/jsc_ctrl_tracker_ai_list.jsx
@@ -12,7 +12,7 @@ import { js_eventEmitter } from '../../js/js_eventEmitter'
 /**
  * props:
  * p_unit
- * 
+ *
  * events:
  * onMakeSwarm
  * onRequestToFollow
@@ -30,12 +30,12 @@ export default class ClssCtrlObjectTrackerAIList extends React.Component {
         };
 
         this.key = Math.random().toString();
-        
+
         js_eventEmitter.fn_subscribe(js_globals.EE_onTrackingAIStatusChanged, this, this.fn_onTrackingAIStatusChanged);
         js_eventEmitter.fn_subscribe(js_globals.EE_onTrackingAIObjectListUpdate, this, this.fn_onTrackingAIObjectListUpdate);
 
         js_globals.v_andruavClient.API_GetTrackingAIClassList(this.props.p_unit,this.state.m_selectedUnits);
-        
+
     }
 
     // --- Core Change: Preventing re-renders from parent props ---
@@ -44,7 +44,7 @@ export default class ClssCtrlObjectTrackerAIList extends React.Component {
         // This effectively ignores prop changes from the parent.
         return (nextState.m_update !== this.state.m_update) || (nextState.m_selectedUnits.length != this.state.m_selectedUnits.length);
     }
-        
+
     componentWillUnmount() {
         js_eventEmitter.fn_unsubscribe (js_globals.EE_onTrackingAIStatusChanged,this);
         js_eventEmitter.fn_unsubscribe (js_globals.EE_onTrackingAIObjectListUpdate,this);
@@ -85,11 +85,11 @@ export default class ClssCtrlObjectTrackerAIList extends React.Component {
     {
         if (p_me.state.m_update === 0) return;
 
-        
+
     }
 
     fnl_trackerAISearch(e) {
-        
+
     }
 
     fnl_trackerAIOff(e) {
@@ -110,35 +110,63 @@ export default class ClssCtrlObjectTrackerAIList extends React.Component {
         }
 
         js_eventEmitter.fn_dispatch(js_globals.EE_onTrackingAIStatusChanged, this.props.p_unit);
-            
+
+    }
+
+    fnl_selectAll(e) {
+        const { m_selectedUnits } = this.state;
+
+
+        const v_units = this.props.p_unit.m_tracker_ai.m_object_list;
+        const len = v_units.length;
+
+        const newSelectedUnits = [];
+        for (let i = 0; i < len; ++i) {
+            newSelectedUnits.push(i);
+        }
+
+        if (newSelectedUnits.length === 0) return; // nothing to select
+
+        this.props.p_unit.m_tracker_ai.m_enable_gui_tracker = true;
+        js_globals.v_andruavClient.API_SendTrackAISelect(this.props.p_unit, newSelectedUnits);
+
+
+        this.setState({
+            m_selectedUnits: newSelectedUnits,
+            m_update: this.state.m_update + 1
+        });
     }
 
 
     fnl_handleCheckboxChange = (event, unitIndex) => {
         const { m_selectedUnits } = this.state;
-        if (event.target.checked) {
-            // Add the unitIndex to the m_selectedUnits array if it's checked
-            this.setState({
-                m_selectedUnits: [...m_selectedUnits, unitIndex]
-            });
-        } else {
-            // Remove the unitIndex from the m_selectedUnits array if it's unchecked
-            this.setState({
-                m_selectedUnits: m_selectedUnits.filter(index => index !== unitIndex)
-            });
-        }
-        // You might want to trigger an action here based on selection change
-        // if (this.props.onUnitsSelected != null)
-        // {
-        //     this.props.onUnitsSelected(this.state.m_selectedUnits);
-        // }
+        let newSelectedUnits;
 
-        this.state.m_selected_units_updated = true;
+        if (event.target.checked) {
+            newSelectedUnits = [...m_selectedUnits, unitIndex];
+        } else {
+            newSelectedUnits = m_selectedUnits.filter(index => index !== unitIndex);
+        }
+
+        // Update the state and then immediately trigger the search
+        this.setState({
+            m_selectedUnits: newSelectedUnits,
+            m_selected_units_updated: true
+        }, () => { // Callback function executed after state update
+            if (newSelectedUnits.length > 0) { // Only send if there are selected units
+                this.props.p_unit.m_tracker_ai.m_enable_gui_tracker = true;
+                js_globals.v_andruavClient.API_SendTrackAISelect(this.props.p_unit, this.state.m_selectedUnits);
+            } else {
+                // Optionally, if no items are selected, you might want to pause tracking
+                this.fnl_trackerAIOff();
+            }
+            js_eventEmitter.fn_dispatch(js_globals.EE_onTrackingAIStatusChanged, this.props.p_unit);
+        });
     };
 
-    
+
     render() {
-        if ((js_siteConfig.CONST_FEATURE.DISABLE_TRACKING_AI != null) 
+        if ((js_siteConfig.CONST_FEATURE.DISABLE_TRACKING_AI != null)
                                 && (js_siteConfig.CONST_FEATURE.DISABLE_TRACKING_AI === false)
                                 && (!this.props.p_unit.m_modules.has_ai_recognition))
         {
@@ -146,12 +174,6 @@ export default class ClssCtrlObjectTrackerAIList extends React.Component {
                         <div className="disabled hidden"/>
                     );
         }
-            
-                    
-        
-
-
-
         if (this.props.p_unit === null || this.props.p_unit === undefined) return;
 
         //CODEBLOCK_START
@@ -160,8 +182,8 @@ export default class ClssCtrlObjectTrackerAIList extends React.Component {
         const c_items = [];
 
         let v_follower_class = "bg-secondry";
-            
-        let css_Track = '';                            
+
+        let css_Track = '';
         let css_Track_title = this.props.title?this.props.title :'object tracker';
         if (this.props.p_unit.m_tracker_ai.m_enable_gui_tracker)
         {
@@ -175,7 +197,7 @@ export default class ClssCtrlObjectTrackerAIList extends React.Component {
             css_Track_title += ' enable tracking ';
             this.state.m_tracker_ai_gui_enabled = false;
         }
-            
+
 
         for (let i = 0; i < len; ++i) {
             const v_unit = v_units[i];
@@ -201,26 +223,25 @@ export default class ClssCtrlObjectTrackerAIList extends React.Component {
 
             return (
                     <div key={'aiq_1' + this.key} className={"btn-group flgtctrlbtn_icon_w " + this.props.className} role="group" aria-label="Button group with nested dropdown">
-                        <button 
+                        <button
                             type="button"
                             className={"btn btn-sm  bi bi-eye-fill " + css_Track}
                             title={css_Track_title}
                             onClick={(e) => this.fnl_trackerAIOnOff(e)}></button>
                         <div key={`aiq_12${this.key}`} className="btn-group" role="group">
-                            <button 
-                                id="btnGroupDrop2"
+                            <button
+                                id={"btnGroupDrop2"+ this.key}
                                 type="button"
                                 className={"btn  btn-sm dropdown-toggle " + v_follower_class}
                                 data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></button>
                             <div
                                 key={'aiq_121' + this.key}
-                                className="dropdown-menu" // This is a core Bootstrap class
-                                aria-labelledby="btnGroupDrop2"
-                                // THIS IS WHERE WE ADDED THE SCROLLING STYLE
+                                className="dropdown-menu"
+                                aria-labelledby={"btnGroupDrop2"+ this.key}
                                 style={{ maxHeight: '200px', overflowY: 'auto' }}
                             >
-                                {c_items} {/* Each c_items is a Bootstrap 'dropdown-item' with 'form-check' */}
-                                <a className="dropdown-item " href="#" onClick={(e) => this.fnl_trackerAIOnOff(e)}>untrack</a>
+                                {c_items}
+                                <a className="dropdown-item " href="#" onClick={(e) => this.fnl_selectAll(e)}>select all</a>
                             </div>
                         </div>
                     </div>
