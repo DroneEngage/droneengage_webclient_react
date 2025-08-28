422abb091b12d1ab6e6c54f54474c8c5f2407682
HefnySco <mohammad.hefny@gmail.com>
2025-08-29 00:49:03 +0300

    build: fix skip logging errors of mavlink file

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,20 +1,9 @@
 import React from 'react';
 
-import { Outlet, Link } from "react-router-dom";
+import { Outlet} from "react-router-dom";
 const Layout = () => {
   return (
     <>
-      {/* <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="planning">Planning</Link>
-          </li>
-        </ul>
-      </nav> */}
-
       <Outlet />
     </>
   )
