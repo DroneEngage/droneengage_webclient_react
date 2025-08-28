5580ccb31e70cad8f8f978ce2e68a38fbeab7b0d
HefnySco <mohammad.hefny@gmail.com>
2025-08-29 02:40:36 +0300

    gui: fix layout

--- src/components/jsc_ctrl_layoutControl.jsx
+++ src/components/jsc_ctrl_layoutControl.jsx
@@ -19,7 +19,7 @@ export class ClssCtrlLayout extends React.Component {
                 <button type="button" id="btn_showSettings" className="btn btn-success btn-sm ctrlbtn bi bi-gear-fill " title='Show/Hide Settings Section' onClick={(e) => fn_showSettings()}><strong> SETTINGS</strong></button>
                 <button type="button" id="btn_showMap" className="btn btn-danger btn-sm ctrlbtn bi bi-map" title='Show Map (ctrl+m)' onClick={(e) => fn_showMap()}><strong> MAP</strong></button>
                 <button type="button" id="btn_showVideo" className="btn btn-warning btn-sm ctrlbtn bi bi-camera-fill" title='Show Video (ctrl+r)' onClick={(e) => fn_showVideoMainTab()}><strong> CAMERA</strong></button>
-                <button type="button" id="btn_showControl" className="btn btn-primary btn-sm ctrlbtn d-none d-sm-inline bi bi-grid-1x2-fill" title='Change Screen Layout (use keys from 1 to 625)' onClick={(e) => fn_showControl(false)}><strong>{' DISPLAY-' + v_display_mode}</strong></button>
+                <button type="button" id="btn_showControl" className="btn btn-primary btn-sm ctrlbtn d-none d-sm-inline bi bi-grid-1x2-fill" title='Change Screen Layout (use keys from 1 to 625)' onClick={(e) => fn_showControl(false)}><strong>{' FORM-' + v_display_mode}</strong></button>
                 <button type="button" id="btn_showControl_small" className="btn btn-primary btn-sm ctrlbtn  d-inline d-sm-none bi bi-grid-1x2-fill" title='Change Screen Layout' onClick={(e) => fn_showControl(true)}><strong>{' display-' + v_display_mode}</strong></button>
             </div>
         );
