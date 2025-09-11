ab9f5ba60e518927cbdba97f521e8039eddd34d4
HefnySco <mohammad.hefny@gmail.com>
2025-09-11 10:17:39 +0300

    complete adding onChangeLanguage Event

--- src/components/gadgets/jsc_language_switcher.jsx
+++ src/components/gadgets/jsc_language_switcher.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
+import {EVENTS as js_event} from '../../js/js_eventList.js'
+import { js_eventEmitter } from '../../js/js_eventEmitter'
+
 export const ClssLanguageSwitcher = () => {
   const { i18n } = useTranslation();
 
@@ -8,6 +11,7 @@ export const ClssLanguageSwitcher = () => {
     i18n.changeLanguage(lang);
     document.documentElement.lang = lang; // Update HTML lang
     document.title = i18n.t('title'); // Update title if needed
+    js_eventEmitter.fn_dispatch(js_event.EE_Language_Changed);
   };
 
   return (
