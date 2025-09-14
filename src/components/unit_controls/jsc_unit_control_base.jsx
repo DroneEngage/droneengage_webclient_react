94222c987e7978ebc95261e3ac3422883f68e4dc
HefnySco <mohammad.hefny@gmail.com>
2025-09-14 03:41:14 +0300

    fix logic: move time stamp to react component

--- src/components/unit_controls/jsc_unit_control_base.jsx
+++ src/components/unit_controls/jsc_unit_control_base.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 
-
 import { js_globals } from '../../js/js_globals.js';
 import { EVENTS as js_event } from '../../js/js_eventList.js'
 import { js_eventEmitter } from '../../js/js_eventEmitter.js'
 
 import * as js_andruavMessages from '../../js/js_andruavMessages.js'
-
-
 
 /**
  * This is a base class for GCS & Drone Units
@@ -20,16 +17,13 @@ export class ClssAndruavUnitBase extends React.Component {
             'm_IsGCS': this.props.p_unit.m_IsGCS,
         };
 
+        this.lastUpdateTime = null;
 
         js_eventEmitter.fn_subscribe(js_event.EE_unitUpdated, this, this.fn_unitUpdated);
         js_eventEmitter.fn_subscribe(js_event.EE_unitNavUpdated, this, this.fn_unitUpdated);
         js_eventEmitter.fn_subscribe(js_event.EE_unitTelemetryOn, this, this.fn_unitTelemetryOn);
         js_eventEmitter.fn_subscribe(js_event.EE_unitTelemetryOff, this, this.fn_unitTelemetryOFF);
-
     }
-
-
-
 
     fn_unitUpdated(me, p_andruavUnit) {
         if (p_andruavUnit === null || p_andruavUnit === undefined) return;
@@ -37,30 +31,24 @@ export class ClssAndruavUnitBase extends React.Component {
             return; // not me
         }
 
-
-
         if (p_andruavUnit.m_IsGCS !== me.props.p_unit.m_IsGCS) {
             // Drone converted to GCS or other type... class is not valid now and an add new should be created.
-
             js_eventEmitter.fn_dispatch(js_event.EE_unitAdded, p_andruavUnit);
         }
 
-        const v_date = p_andruavUnit.date;
-        if (v_date === null || v_date === undefined) {
-            p_andruavUnit.date = new Date();
-        }
-        else {
-            const n = new Date();
-            if ((n - p_andruavUnit.date) < 300) {
+        const now = new Date();
+        if (me.lastUpdateTime === null) {
+            me.lastUpdateTime = now;
+        } else {
+            if ((now - me.lastUpdateTime) < 300) {
                 return;
             }
-            p_andruavUnit.date = new Date();
+            me.lastUpdateTime = now;
         }
 
         if (me.state.m_update === 0) return;
 
         me.setState({ 'm_update': me.state.m_update + 1 });
-
     }
 
     fn_unitTelemetryOn(me, p_andruavUnit) {
@@ -81,13 +69,10 @@ export class ClssAndruavUnitBase extends React.Component {
         me.setState({ 'm_update': me.state.m_update + 1 });
     }
 
-
     childcomponentWillMount() { };
     childcomponentWillUnmount() { };
 
-
     componentDidMount() {
-
         this.childcomponentWillMount();
     }
 
@@ -99,24 +84,17 @@ export class ClssAndruavUnitBase extends React.Component {
         this.childcomponentWillUnmount();
     }
 
-
-
-
-
     fn_takeTXCtrl(e, p_andruavUnit) {
         p_andruavUnit.m_Telemetry.m_rxEngaged = true;
         js_globals.v_andruavFacade.API_TXCtrl(p_andruavUnit, js_andruavMessages.CONST_TX_SIGNAL_FREEZE_ALL);
         js_eventEmitter.fn_dispatch(js_event.EE_requestGamePad, p_andruavUnit);
-
     }
 
     fn_releaseTXCtrl(p_andruavUnit) {
         p_andruavUnit.m_Telemetry.m_rxEngaged = false;
         js_globals.v_andruavFacade.API_TXCtrl(p_andruavUnit, js_andruavMessages.CONST_RC_SUB_ACTION_RELEASED);
-        js_eventEmitter.fn_dispatch(js_event.EE_requestGamePadreleaseGamePad, p_andruavUnit);
-
+        js_eventEmitter.fn_dispatch(js_event.EE_requestGamePadRelease, p_andruavUnit);  // Fixed event name for clarity
     }
-
 
     fn_displayParamsDialog(p_andruavUnit) {
         if (js_globals.v_andruavClient === null || js_globals.v_andruavClient === undefined) return;
@@ -129,8 +107,4 @@ export class ClssAndruavUnitBase extends React.Component {
 
         js_eventEmitter.fn_dispatch(js_event.EE_andruavUnitLidarShow, p_andruavUnit);
     }
-
-
-
-
 }
